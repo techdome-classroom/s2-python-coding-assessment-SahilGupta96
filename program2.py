4e1a40b6f56cd739a7ed0906ff5f4e02613a1fb3
SahilGupta96 <135022668+SahilGupta96@users.noreply.github.com>
2024-09-27 14:53:14 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,83 +1,56 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        
+        roman_map = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        total = 0
+        n = len(s)
+        
+        for i in range(n):
+            
+            if i < n - 1 and roman_map[s[i]] < roman_map[s[i + 1]]:
+                total -= roman_map[s[i]]  
+            else:
+                total += roman_map[s[i]]  
+                
+        return total
 
-def value(r):
-	if (r == 'I'):
-		return 1
-	if (r == 'V'):
-		return 5
-	if (r == 'X'):
-		return 10
-	if (r == 'L'):
-		return 50
-	if (r == 'C'):
-		return 100
-	if (r == 'D'):
-		return 500
-	if (r == 'M'):
-		return 1000
-	return -1
 
-def romanToInt( s: str) -> int:
-    
-	res = 0
-	i = 0
+import unittest
 
-	while (i < len(s)):
+class TestRomanToInt(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
 
-		# Getting value of symbol s[i]
-		s1 = value(s[i])
+    def test_example1(self):
+        self.assertEqual(self.solution.romanToInt("III"), 3)
 
-		if (i + 1 < len(s)):
+    def test_example2(self):
+        self.assertEqual(self.solution.romanToInt("LVIII"), 58)
 
-			# Getting value of symbol s[i + 1]
-			s2 = value(s[i + 1])
+    def test_example3(self):
+        self.assertEqual(self.solution.romanToInt("MCMXCIV"), 1994)
 
-			# Comparing both values
-			if (s1 >= s2):
+    def test_single_roman_digit(self):
+        self.assertEqual(self.solution.romanToInt("X"), 10)
 
-				# Value of current symbol is greater
-				# or equal to the next symbol
-				res = res + s1
-				i = i + 1
-			else:
+    def test_subtraction_rule(self):
+        self.assertEqual(self.solution.romanToInt("IV"), 4)
+        self.assertEqual(self.solution.romanToInt("IX"), 9)
 
-				# Value of current symbol is greater
-				# or equal to the next symbol
-				res = res + s2 - s1
-				i = i + 2
-		else:
-			res = res + s1
-			i = i + 1
+    def test_large_number(self):
+        self.assertEqual(self.solution.romanToInt("MMMCMXCIX"), 3999)
 
-	return res
-import unittest  
+    def test_empty_string(self):
+        self.assertEqual(self.solution.romanToInt(""), 0)
 
-class TestRomanToInt(unittest.TestCase):  
-    def setUp(self):  
-        self.solution = Solution()  
-
-    def test_example1(self):  
-        self.assertEqual(self.solution.romanToInt("III"), 3)  
-
-    def test_example2(self):  
-        self.assertEqual(self.solution.romanToInt("LVIII"), 58)  
-
-    def test_example3(self):  
-        self.assertEqual(self.solution.romanToInt("MCMXCIV"), 1994)  
-
-    def test_single_roman_digit(self):  
-        self.assertEqual(self.solution.romanToInt("X"), 10)  
-
-    def test_subtraction_rule(self):  
-        self.assertEqual(self.solution.romanToInt("IV"), 4)  
-        self.assertEqual(self.solution.romanToInt("IX"), 9)  
-
-    def test_large_number(self):  
-        self.assertEqual(self.solution.romanToInt("MMMCMXCIX"), 3999)  
-
-    def test_empty_string(self):  
-        # Remove this test if we assume input will always be valid as per constraints  
-        self.assertEqual(self.solution.romanToInt(""), 0)  
-
-if __name__ == '_main_':  
+if __name__ == '__main__':
     unittest.main(argv=['first-arg-is-ignored'], exit=False)
-
