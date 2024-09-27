b385e03b8c20e56ffccc4d1c3e8df2a3b0dc23a3
SahilGupta96 <135022668+SahilGupta96@users.noreply.github.com>
2024-09-27 14:46:12 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,28 +1,54 @@
 
-def romanToInt(self, s: str) -> int:
-        
-        roman_map = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
-        }
-        
+def value(r):
+	if (r == 'I'):
+		return 1
+	if (r == 'V'):
+		return 5
+	if (r == 'X'):
+		return 10
+	if (r == 'L'):
+		return 50
+	if (r == 'C'):
+		return 100
+	if (r == 'D'):
+		return 500
+	if (r == 'M'):
+		return 1000
+	return -1
 
-        i = 0
-        num = 0
-        while i < len(s):
-            if i+1<len(s) and s[i:i+2] in roman_map:
-                num+=roman_map[s[i:i+2]]
-                i+=2
-            else:
-                #print(i)
-                num+=roman_map[s[i]]
-                i+=1
-        return num
+def romanToInt( s: str) -> int:
+    
+	res = 0
+	i = 0
+
+	while (i < len(s)):
+
+		# Getting value of symbol s[i]
+		s1 = value(s[i])
+
+		if (i + 1 < len(s)):
+
+			# Getting value of symbol s[i + 1]
+			s2 = value(s[i + 1])
+
+			# Comparing both values
+			if (s1 >= s2):
+
+				# Value of current symbol is greater
+				# or equal to the next symbol
+				res = res + s1
+				i = i + 1
+			else:
+
+				# Value of current symbol is greater
+				# or equal to the next symbol
+				res = res + s2 - s1
+				i = i + 2
+		else:
+			res = res + s1
+			i = i + 1
+
+	return res
 import unittest  
 
 class TestRomanToInt(unittest.TestCase):  
