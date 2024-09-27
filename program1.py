eeb1e95f1c6718c63c6545c7a9051094a66e2da3
SahilGupta96 <135022668+SahilGupta96@users.noreply.github.com>
2024-09-27 15:08:13 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -15,28 +15,35 @@ class Solution(object):
 
 import unittest
 
-class TestSolution(unittest.TestCase):
+
+class TestRomanToInt(unittest.TestCase):
     def setUp(self):
         self.solution = Solution()
 
-    def test_valid_parentheses(self):
-        self.assertTrue(self.solution.isValid("()"))
-        self.assertTrue(self.solution.isValid("()[]{}"))
-        self.assertTrue(self.solution.isValid("{[()]}"))
+    def test_example1(self):
+        self.assertEqual(self.solution.romanToInt("III"), 3)
 
-    def test_invalid_parentheses(self):
-        self.assertFalse(self.solution.isValid("(]"))
-        self.assertFalse(self.solution.isValid("([)]"))
+    def test_example2(self):
+        self.assertEqual(self.solution.romanToInt("LVIII"), 58)
+
+    def test_example3(self):
+        self.assertEqual(self.solution.romanToInt("MCMXCIV"), 1994)
+
+    def test_single_roman_digit(self):
+        self.assertEqual(self.solution.romanToInt("X"), 10)
+
+    def test_subtraction_rule(self):
+        self.assertEqual(self.solution.romanToInt("IV"), 4)
+        self.assertEqual(self.solution.romanToInt("IX"), 9)
+
+    def test_large_number(self):
+        self.assertEqual(self.solution.romanToInt("MMMCMXCIX"), 3999)
 
     def test_empty_string(self):
-        self.assertTrue(self.solution.isValid(""))
+        self.assertEqual(self.solution.romanToInt(""), 0)
 
-    def test_mixed_parentheses(self):
-        self.assertFalse(self.solution.isValid("(){"))
-
-if _name_ == '_main_':
+if __name__ == '__main__':
     unittest.main(argv=['first-arg-is-ignored'], exit=False)
-
     
 
 
