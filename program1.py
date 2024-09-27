7179a5f0adacc56f1ca5b1cb062210751feece27
SahilGupta96 <135022668+SahilGupta96@users.noreply.github.com>
2024-09-27 15:07:03 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,19 +1,17 @@
 class Solution(object):
     def isValid(self, s):
-        stack = [] # create an empty stack to store opening brackets
-        for c in s: # loop through each character in the string
-            if c in '([{': # if the character is an opening bracket
-                stack.append(c) # push it onto the stack
-            else: # if the character is a closing bracket
+        stack = []
+        for c in s: 
+            if c in '([{': 
+                stack.append(c) 
+            else: 
                 if not stack or \
                     (c == ')' and stack[-1] != '(') or \
                     (c == '}' and stack[-1] != '{') or \
                     (c == ']' and stack[-1] != '['):
-                    return False # the string is not valid, so return false
-                stack.pop() # otherwise, pop the opening bracket from the stack
-        return not stack # if the stack is empty, all opening brackets have been matched with their corresponding closing brackets,
-                         # so the string is valid, otherwise, there are unmatched opening brackets, so return false
-
+                    return False 
+                stack.pop() 
+        return not stack
 
 import unittest
 
